c43b890b81ff6d6c592ffb243e6cfb2d079b7d21
hendrixxD <maticsjnr@gmail.com>
2023-10-02 11:10:12 +0100

    moved requirements.txt to project folder for deployment

--- CHROME_EXT/chrome_ext_app/serializers.py
+++ CHROME_EXT/chrome_ext_app/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import *
 import uuid
 
+
 class CreateRecordingSerializer(serializers.ModelSerializer):
     name = serializers.CharField(read_only=True)
     class Meta:
@@ -15,14 +16,14 @@ class CreateRecordingSerializer(serializers.ModelSerializer):
         recordings = Recordings.objects.create(name=uuid.uuid4(), **validated_data)
         return recordings
 
+
 class GetRecordingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recordings
         fields = ['id', 'name', 'title', 'transcript', 'video', 'created_at']
-        
+
+
 class GetRecordingVideoSerializer(serializers.Serializer):
     recording = GetRecordingSerializer()
     # videos = VideoSerializers(many=True)
-
-
 
