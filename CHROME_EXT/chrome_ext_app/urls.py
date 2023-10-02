94eec1332caf9d3f64b21a079c9130885960d459
hendrixxD <maticsjnr@gmail.com>
2023-10-02 13:05:53 +0100

    added an endpoint

--- CHROME_EXT/chrome_ext_app/urls.py
+++ CHROME_EXT/chrome_ext_app/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     
     path('create_video/', views.CreateRecordingView.as_view()),
     path('all_records/', views.AllRecordingsView.as_view()),
-    path('save-data/<int:id>/', views.GetVideoDataView.as_view()),
+    path('savedata/<int:id>/', views.GetVideoDataView.as_view()),
     path('singlevideo/<int:id>/', views.GetSingleVideoView.as_view()),
+    path('mergedata/<int:id>/', MergeRecordingView.as_view()),
     
     # path('playback/<uuid:unique_identifier>/', SingleVideoView.as_view(), name='video_playback'),
 ]
